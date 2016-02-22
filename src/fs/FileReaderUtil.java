2dca5e148cf677a6dc5f150a4cb1a19bf6392da5
BugDiver <vinaysh@thoughtworks.com>
2016-02-22 14:06:10 +0530

    [Vinay]:- added a FileReader class .
    What changes have you made?
    
    * wrote a program to read the given file and store contenets as string
    * and return the content.
    
    Why do we need this change?
    
    * we need to store content so we can do some operation on that.
    
    What can this change impact?
    
    *

---
+++ src/fs/FileReaderUtil.java
@@ -0,0 +1,23 @@
+package fs;
+
+import java.io.*;
+
+class FileReaderUtil{
+	public String readFile(File filename) {
+        String line;
+        StringBuilder sb = new StringBuilder("");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\r\n");
+            }
+        } catch (FileNotFoundException e){
+            System.err.println("File not Found");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("unable to Read");
+            System.exit(1);
+        }
+        return sb.toString().trim();
+    }
+}
