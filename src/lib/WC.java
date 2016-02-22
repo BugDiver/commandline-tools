e7e03d6b80637aa790f80cd13d3f042baef36610
BugDiver <vinaysh@thoughtworks.com>
2016-02-22 15:39:20 +0530

    [Vinay]:- create library for word count.
    What changes have you made?
    
    * wrote some functions to do operations on file like count words , lines
    * and charecters.
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- src/lib/WC.java
+++ src/lib/WC.java
@@ -1,13 +1,35 @@
 package lib;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class WC{
+	private String fileName;
 	private String content;
 
-	public WC(String text){
+	public WC(String text,String fileName){
+		this.fileName = fileName;
 		this.content = text;
 	}
 
 	public int countChars(){
 		return content.length();
+	}
+
+	public int countWords(){
+		return content.split("\\W+").length;
+	}
+
+	public int countLines(){
+		return content.split("\\n+").length-1;
+	}
+
+	public String formatedResult(){
+		StringBuilder sb = new StringBuilder(""); 
+		sb.append("\t").append(countLines());
+        sb.append("\t").append(countWords());
+        sb.append("\t").append(countChars());
+        sb.append("\t").append(fileName);
+        return sb.toString();
 	}
 }
--- test/lib/WCTest.java
+++ test/lib/WCTest.java
@@ -5,10 +5,39 @@ import static org.junit.Assert.*;
 
 public class WCTest {
 	@Test
-	public void tstForCountingCharactersOfAGivenString() {
+	public void testForCountingCharactersOfAGivenString() {
 		String text = "Hello world!!!!";
-		WC wc = new WC(text);
+		WC wc = new WC(text,"foo");
 
 		assertEquals(wc.countChars() ,15);
+	}
+
+	@Test
+	public void testForCountingWordsOfAGivenString() {
+		String text = "Hello world!!!!";
+		WC wc = new WC(text,"bar");
+		assertEquals(wc.countWords() ,2);
+	}
+
+	@Test
+	public void testForCountingLinessOfAGivenString() {
+		String text = "Hello world!!!";
+		WC wc = new WC(text,"whyYouNeed");
+		assertEquals(wc.countLines() ,0);
+	}
+
+	@Test
+	public void testForCountingLinessOfAGivenStringWithMultipleLines() {
+		String text = "Hello world\nHow are you?";
+		WC wc = new WC(text,"getLost");
+		assertEquals(wc.countLines() ,1);
+	}
+
+	@Test
+	public void testForFormatedResultOfAGivenString() {
+		String text = "Hello world!!\nHow are you?";
+		WC wc = new WC(text,"something");
+		String expected = "\t1\t5\t26\tsomething";
+		assertEquals(wc.formatedResult() , expected);
 	}
 }
